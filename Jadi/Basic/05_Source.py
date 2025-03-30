df9dcf26b1e31811f055d67f3c84be10e9aabf12
Danial Forouzanfar <88351867+Dforouzanfar@users.noreply.github.com>
2025-03-30 11:56:24 +0000

    Basic_chapter5

--- Jadi/Basic/05_Source.py
+++ Jadi/Basic/05_Source.py
@@ -5,29 +5,80 @@ from collections import OrderedDict
 def calculate_averages(input_file_name, output_file_name):
     reader = csv.reader(open(input_file_name))
     writer = csv.writer(open(output_file_name, 'w', newline=''))
-    mydict = OrderedDict()
+
+    for line in reader:
+        name = line[0]
+        grades = list(map(float, line[1:]))
+        gpa = str(mean(grades))
+
+        writer.writerow([name,gpa])
+
+
+
+def calculate_sorted_averages(input_file_name, output_file_name):
+    reader = csv.reader(open(input_file_name))
+    writer = csv.writer(open(output_file_name, 'w', newline=''))
+    mylist = []
 
     for line in reader:
         name = line[0]
         grades = list(map(float, line[1:]))
         gpa = mean(grades)
-        mydict[name] = str(gpa)
+        mylist.append((name, gpa))
+    
+    mylist.sort()
+    mylist.sort(key=lambda x: x[1])
 
-        writer.writerow(f"{name},str(gpa)")
-
-
-
-def calculate_sorted_averages(input_file_name, output_file_name):
+    for i, j in mylist:
+        writer.writerow([i,j])
     
 
 
 def calculate_three_best(input_file_name, output_file_name):
+    reader = csv.reader(open(input_file_name))
+    writer = csv.writer(open(output_file_name, 'w', newline=''))
+    mylist = []
+
+    for line in reader:
+        name = line[0]
+        grades = list(map(float, line[1:]))
+        gpa = mean(grades)
+        mylist.append((name, gpa))
     
+    mylist.sort()
+    mylist.sort(key=lambda x: x[1], reverse=True)
+
+    for i, j in mylist[:3]:
+        writer.writerow([i,j])
 
 
 def calculate_three_worst(input_file_name, output_file_name):
+    reader = csv.reader(open(input_file_name))
+    writer = csv.writer(open(output_file_name, 'w', newline=''))
+    mylist = []
+
+    for line in reader:
+        name = line[0]
+        grades = list(map(float, line[1:]))
+        gpa = mean(grades)
+        mylist.append((name, gpa))
     
+    mylist.sort()
+    mylist.sort(key=lambda x: x[1])
+
+    for i, j in mylist[:3]:
+        writer.writerow([j])
 
 
 def calculate_average_of_averages(input_file_name, output_file_name):
+    reader = csv.reader(open(input_file_name))
+    writer = csv.writer(open(output_file_name, 'w', newline=''))
+    mylist = []
+
+    for line in reader:
+        grades = list(map(float, line[1:]))
+        gpa = mean(grades)
+        mylist.append(gpa)
     
+    gpa = mean(mylist)
+    writer.writerow(gpa)
